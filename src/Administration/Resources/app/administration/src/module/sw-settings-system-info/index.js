9be8c3ca8b4769885f97f6de62e6d17ffe3e6cd6
Alexander Kluth <alexander.kluth@pixolith.de>
2020-05-14 12:53:51 +0200

    Introduce sw-setting-system-info core plugin
    Signed-off-by: Alexander Kluth <alexander.kluth@pixolith.de>

---
+++ src/Administration/Resources/app/administration/src/module/sw-settings-system-info/index.js
@@ -0,0 +1,31 @@
+import './page/sw-settings-system-info';
+
+const { Module } = Shopware;
+
+Module.register('sw-settings-system-info', {
+    type: 'core',
+    name: 'settings-system-info',
+    title: 'sw-settings-system-info.general.mainMenuItemGeneral',
+    description: 'sw-settings-system-info.general.description',
+    version: '1.0.0',
+    targetVersion: '1.0.0',
+    color: '#9AA8B5',
+    icon: 'default-action-settings',
+    favicon: 'icon-module-settings.png',
+
+    routes: {
+        index: {
+            component: 'sw-settings-system-info',
+            path: 'index',
+            meta: {
+                parentPath: 'sw.settings.index'
+            }
+        }
+    },
+
+    settingsItem: {
+        group: 'system',
+        to: 'sw.settings.system-info.index',
+        icon: 'default-symbol-products'
+    }
+});
